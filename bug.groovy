04e5fb0113185b252deb8953c8b1f7d96a3a454c
BugHunter-X <askzenai@gmail.com>
2025-01-05 02:04:04 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,12 @@
+```groovy
+def methodWithClosure(closure) {
+  println "Before closure execution"
+  closure()
+  println "After closure execution"
+}
+
+methodWithClosure { 
+  println "Inside closure"
+  throw new RuntimeException("Something went wrong!")
+}
+```
